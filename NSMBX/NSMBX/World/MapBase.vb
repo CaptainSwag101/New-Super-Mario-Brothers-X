273c44fbfa366ba3aa8e218f00c451ae34189adc
James Pelster <jpmac26@gmail.com>
2014-02-06 15:29:47 -0800

    Remove unnecessary variables

--- NSMBX/NSMBX/World/MapBase.vb
+++ NSMBX/NSMBX/World/MapBase.vb
@@ -1,8 +1,6 @@
 ﻿Public Class MapBase
     ' MAP LISTS
     Public TileList(0, 0) As Tile
-    Public AnimationList As New List(Of Tile)
-    'Public NPCList As New List(Of NPC)
 
     Public MapName As String = ""
     Public MapWidth As Integer = 0
@@ -10,9 +8,6 @@
 
     ' PLAYER START LOCATION
     Public StartLocation As Vector2
-
-    ' NUMBER OF NPCs
-    Public NPCCount As Integer = 0
 
     Public Sub New(Name As String, width As Integer, height As Integer, start As Vector2)
         ReDim TileList(width, height)
