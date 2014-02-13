6a01f03a9e48c507c4354fe1e19fc272d5056859
James Pelster <jpmac26@gmail.com>
2014-02-13 15:53:30 -0800

    Update Player.vb

--- NSMBX/NSMBX/Globals/Player.vb
+++ NSMBX/NSMBX/Globals/Player.vb
@@ -1,5 +1,21 @@
-﻿Public Class Player
+Public Class Player
+    Public CurrentForm As Powerup
+    Public LastForm As Powerup
+    Public ReserveItem As NPC
+    
     Public Sub New()
-        
+        If Globals.NewGame
     End Sub
+    
 End Class
+
+Public Enum Powerup
+    None
+    Mushroom
+    Fire
+    Leaf
+    Tanooki
+    Statue
+    Hammer
+    Ice
+End Enum
